4451cf60127a463018930c8c3cd35187d284992c
Shea Parkes <shea.parkes@milliman.com>
2022-12-08 20:13:41 -0500

    Add point-free nonsense.
    Do this for entertainment.

--- advent2022/day1/task1.py
+++ advent2022/day1/task1.py
@@ -5,3 +5,27 @@ clean = [list(map(int, x.split())) for x in raw.rstrip().split("\n\n")]
 elfs = list(map(sum, clean))
 max(elfs)
 sum(sorted(elfs)[-3:])
+
+
+import cytoolz.curried as z
+
+elves = z.pipe(
+    raw,
+    str.rstrip,
+    z.flip(str.split)("\n\n"),
+    z.map(
+        z.compose_left(
+            str.split,
+            z.map(int),
+            sum,
+        ),
+    ),
+    list,
+)
+
+max(elves)
+z.pipe(
+    elves,
+    z.topk(3),
+    sum,
+)
