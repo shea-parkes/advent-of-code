22ce4e30af538b374ce4a909bc56646711d2cfcd
Shea Parkes <shea.parkes@milliman.com>
2022-12-11 14:59:57 -0500

    Cleanup day11

--- advent2022/day11/day11.py
+++ advent2022/day11/day11.py
@@ -7,6 +7,14 @@ import typing
 import cytoolz.curried as z
 
 
+def is_divisible(denom):
+    """Build a divisibility checker"""
+    return z.compose_left(
+        z.flip(op.mod)(denom),
+        op.not_,
+    )
+
+
 @dataclasses.dataclass
 class Monkey:
     """Silly class"""
@@ -38,56 +46,56 @@ MONKEYS = (
     Monkey(
         items=collections.deque((93, 98)),
         operation=z.curry(op.mul)(17),
-        test=lambda x: not x % 19,
+        test=is_divisible(19),
         target_true=5,
         target_false=3,
     ),
     Monkey(
         items=collections.deque((95, 72, 98, 82, 86)),
         operation=z.curry(op.add)(5),
-        test=lambda x: not x % 13,
+        test=is_divisible(13),
         target_true=7,
         target_false=6,
     ),
     Monkey(
         items=collections.deque((85, 62, 82, 86, 70, 65, 83, 76)),
         operation=z.curry(op.add)(8),
-        test=lambda x: not x % 5,
+        test=is_divisible(5),
         target_true=3,
         target_false=0,
     ),
     Monkey(
         items=collections.deque((86, 70, 71, 56)),
         operation=z.curry(op.add)(1),
-        test=lambda x: not x % 7,
+        test=is_divisible(7),
         target_true=4,
         target_false=5,
     ),
     Monkey(
         items=collections.deque((77, 71, 86, 52, 81, 67)),
         operation=z.curry(op.add)(4),
-        test=lambda x: not x % 17,
+        test=is_divisible(17),
         target_true=1,
         target_false=6,
     ),
     Monkey(
         items=collections.deque((89, 87, 60, 78, 54, 77, 98)),
         operation=z.curry(op.mul)(7),
-        test=lambda x: not x % 2,
+        test=is_divisible(2),
         target_true=1,
         target_false=4,
     ),
     Monkey(
         items=collections.deque((69, 65, 63)),
         operation=z.curry(op.add)(6),
-        test=lambda x: not x % 3,
+        test=is_divisible(3),
         target_true=7,
         target_false=2,
     ),
     Monkey(
         items=collections.deque((89,)),
         operation=z.flip(op.pow)(2),
-        test=lambda x: not x % 11,
+        test=is_divisible(11),
         target_true=0,
         target_false=2,
     ),
@@ -99,7 +107,7 @@ for _ in range(10_000):
 
 z.pipe(
     MONKEYS,
-    z.map(z.flip(getattr, "throw_count")),
+    z.map(z.flip(getattr)("throw_count")),
     z.topk(2),
     z.reduce(op.mul),
 )
